c4d63e60573402eb3b214b94f09f9bc1dc2c1090
kunnaffa <95153455+kunnaffa@users.noreply.github.com>
2023-06-17 01:41:47 +0300

    Update lan.py
    commented the code

--- lan.py
+++ lan.py
@@ -1,26 +1,33 @@
 import tkinter as tk
 import subprocess
 
+# Create the root window
 root = tk.Tk()
 root.title("Language Switcher")
 
+# Function to switch to Hebrew language
 def switch_to_hebrew():
     subprocess.run(['setxkbmap', 'il'])
 
+# Function to switch to English language
 def switch_to_english():
     subprocess.run(['setxkbmap', 'us'])
 
+# Function to switch to Arabic language
 def switch_to_arabic():
     subprocess.run(['setxkbmap', 'ar'])
-    
-hebrew_button = tk.Button(root, text="Hebrew",cursor="hand2", command=switch_to_hebrew)
+
+# Create a button for Hebrew language
+hebrew_button = tk.Button(root, text="Hebrew", cursor="hand2", command=switch_to_hebrew)
 hebrew_button.pack()
 
-
-english_button = tk.Button(root, text="English",cursor="hand2", command=switch_to_english)
+# Create a button for English language
+english_button = tk.Button(root, text="English", cursor="hand2", command=switch_to_english)
 english_button.pack()
 
-arabic_button = tk.Button(root, text='Arabic',cursor="hand2", command=switch_to_arabic)
+# Create a button for Arabic language
+arabic_button = tk.Button(root, text='Arabic', cursor="hand2", command=switch_to_arabic)
 arabic_button.pack()
 
+# Start the main event loop
 root.mainloop()
